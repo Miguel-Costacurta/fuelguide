8a8e42054f04e6d9304c07526491f9a0709a3173
Miguel Costacurta <miguel.costacurta@intranet.dicomp.com.br>
2026-05-26 16:26:18 -0300

    mais 2 regras de negocio para filtros

--- api/src/main/java/com/fuelguide/api/posto/PostoService.java
+++ api/src/main/java/com/fuelguide/api/posto/PostoService.java
@@ -2,6 +2,7 @@ package com.fuelguide.api.posto;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -29,22 +30,29 @@ public class PostoService {
     }
 
     public List<PostoEntity> buscarMaisBaratoPorCombustivel(ETipoCombustivel tipo){
-
-        List<PostoEntity> postos = iPostoRepository.findAll();
-
-        return postos.stream()
-                .filter(p -> p.getPrecos().stream()
-                        .anyMatch(preco -> preco.getTipo() == tipo))
-                .sorted((p1,p2) -> {
-                    double v1 = p1.getPrecos().stream()
-                            .filter(preco -> preco.getTipo() == tipo)
-                            .findFirst().get().getValor();
-                    double v2 = p2.getPrecos().stream()
-                            .filter(preco -> preco.getTipo() == tipo)
-                            .findFirst().get().getValor();
-                    return Double.compare(v1,v2);
-
-                })
+        return iPostoRepository.findAll().stream()
+                .filter(p ->p.getPrecos() != null && !p.getPrecos().isEmpty())
+                .filter(p -> p.getPrecos().stream().anyMatch(x -> x.getTipo() == tipo))
+                .sorted(compararPorPreco(tipo))
                 .toList();
+    }
+
+    public PostoEntity buscarPostoMaisBarato(String cidade, ETipoCombustivel tipo){
+        return iPostoRepository.findByCidade(cidade).stream()
+                .filter(p-> p.getPrecos() != null && !p.getPrecos().isEmpty())
+                .min(compararPorPreco(tipo))
+                .orElse(null);
+    }
+
+//-------------- HELPERS -----------------------
+    private double getPreco(PostoEntity posto, ETipoCombustivel tipo){
+        return posto.getPrecos().stream()
+                .filter(p -> p.getTipo() == tipo)
+                .map(PrecoCombustivel::getValor)
+                .findFirst()
+                .orElse(Double.MAX_VALUE);
+    }
+    private Comparator<PostoEntity> compararPorPreco(ETipoCombustivel tipo){
+        return Comparator.comparing(posto -> getPreco(posto, tipo));
     }
 }
